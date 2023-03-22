90baf3f2eed45cfea12aac0cfb4ffaef271f00dd
ddKirill <cfaleev@yandex.ru>
2023-03-22 05:54:31 +0500

    Initial commit

---
+++ src/main/java/com/ddkirill/ratesbot/RatesBotApplication.java
@@ -0,0 +1,13 @@
+package com.ddkirill.ratesbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatesBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatesBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddkirill/ratesbot/RatesBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddkirill.ratesbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatesBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
